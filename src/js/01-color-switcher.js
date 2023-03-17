803e18debdf5f9a8b49ed2ec71ae1bd71fd328ff
Natali <114168847+Natali9108@users.noreply.github.com>
2023-03-17 11:51:45 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,48 @@
-const date = new Date();
-console.log(date);
-// console.log(date.getTime());
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', handelStartBtn);
+refs.stopBtn.addEventListener('click', handelStopBtn);
+
+const changeBodyColor = {
+  intervalId: null,
+  isActive: false,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    this.intervalId = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+      refs.startBtn.disabled = true;
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+  },
+};
+
+function handelStartBtn() {
+  changeBodyColor.start();
+}
+
+function handelStopBtn() {
+  changeBodyColor.stop();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
